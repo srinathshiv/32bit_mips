a9f470f8c9de85c49dcaf91e32bd63b677ff95c1
srinathshiv <srinathshiv94@gmail.com>
2017-04-20 01:55:33 -0700

    dCache pins updated to reflect correct logic

--- dCache.sv
+++ dCache.sv
@@ -4,7 +4,7 @@ module dCache(
 	input logic [31:0]dCacheAddr, 
 	input logic dCacheWriteEn, 
 	input logic dCacheReadEn,
-	input logic [31:0]rfReadData_p1,
+	input logic [31:0]dCacheWriteData,
 	output logic [31:0]dCacheReadData);
 
 reg [31:0]dcache[256:0];
@@ -13,7 +13,7 @@ assign dCacheReadData = (dCacheReadEn == 1'b1) ? dcache[dCacheAddr] : 1'b0 ;
    
 always @(posedge clk) begin
      if (dCacheWriteEn) begin
-     dcache[dCacheAddr] <= rfReadData_p1;
+     dcache[dCacheAddr] <= dCacheWriteData;
      end
 end
 
